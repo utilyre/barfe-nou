0accc07b84d2b7a983c21b9725e9d43c31ca8f1c
Amirabbas Fazelinia <utilyre@gmail.com>
2024-07-31 16:16:45 +0330

    build: setup golang, pnpm, and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['public/**/*.{html,js}', 'components/**/*.templ'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
